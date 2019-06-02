7c884c750aa77458b9424cdd2d628b1298665895
cyhhao <516365710@qq.com>
2019-06-02 13:06:31 +0800

    Update install-python3.sh

--- install-python3.sh
+++ install-python3.sh
@@ -1,7 +1,7 @@
 yum -y groupinstall "Development tools"
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
-tar -xvJf  Python-3.7.0.tgz
+tar zxvf  Python-3.7.0.tgz
 cd Python-3.7.0
 ./configure --prefix=/usr/local/bin/python3
 make
