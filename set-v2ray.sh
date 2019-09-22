f9fc06ca3c585efa9d773f60e177c6fd65dd5290
cyh <cyh@zhusun.in>
2019-09-22 23:27:28 +0800

    fix bug

--- set-v2ray.sh
+++ set-v2ray.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-read -p "Enter your ssl_certificate path:"
-ssl_certificate_path=$REPLY
+#read -p "Enter your ssl_certificate path:"
+#ssl_certificate_path=$REPLY
+#
+#read -p "Enter your ssl_certificate key path:"
+#ssl_certificate_key_path=$REPLY
 
-read -p "Enter your ssl_certificate key path:"
-ssl_certificate_key_path=$REPLY
-
-read -p "Enter your v2ray port:"
+read -p "Enter your v2ray server port:"
 port=$REPLY
 
 read -p "Enter your server name(s) space separated:"
@@ -25,8 +25,7 @@ v2ui_port=$REPLY
 cat << EOF > /etc/nginx/conf.d/v2ray.conf
 server {
     listen  443 ssl http2 default_server;
-    ssl_certificate       $ssl_certificate_path; #fullchain
-    ssl_certificate_key   $ssl_certificate_key_path; #key
+
     ssl_protocols         TLSv1.2;
     ssl_ciphers           HIGH:!aNULL:!MD5;
     server_name           $servers;
@@ -48,21 +47,19 @@ server {
 
 server {
     listen  443 ssl http2 ;
-    ssl_certificate       $ssl_certificate_path; # managed by Certbot
-    ssl_certificate_key   $ssl_certificate_key_path; # managed by Certbot
+
     ssl_protocols         TLSv1.2;
     ssl_ciphers           HIGH:!aNULL:!MD5;
     server_name           $v2ui_server_name;
-    add_header            Strict-Transport-Security "max-age=63072000; includeSubdomains; ";
     add_header            Strict-Transport-Security "max-age=63072000; includeSubdomains; ";
 
     location / {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:$v2ui_port;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host $http_host;
+        proxy_set_header Host \$http_host;
     }
 
 
