99df73724be497caca3f0711ef27e9d2bb4e3e8d
cyh <cyh@zhusun.in>
2020-11-07 00:08:46 +0800

    upgrade python

--- install-python3.sh
+++ install-python3.sh
@@ -1,8 +1,9 @@
 yum -y groupinstall "Development tools"
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
-wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
-tar zxvf  Python-3.7.4.tgz
-cd Python-3.7.4
+yum install wget git
+wget https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tgz
+tar zxvf  Python-3.8.6.tgz
+cd Python-3.8.6
 ./configure --prefix=/usr/local/bin/python3
 make
 make install
