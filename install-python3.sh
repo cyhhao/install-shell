a0e33a7bc5848a8b7e42f15343f0a59519db3326
Call White <cyh@zhusun.in>
2022-12-09 15:49:50 +0800

    Update install-python3.sh

--- install-python3.sh
+++ install-python3.sh
@@ -1,9 +1,9 @@
 yum -y groupinstall "Development tools"
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
 yum install wget git
-wget https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tgz
-tar zxvf  Python-3.8.6.tgz
-cd Python-3.8.6
+wget https://www.python.org/ftp/python/3.11.1/Python-3.11.1.tgz
+tar zxvf Python-3.11.1.tgz
+cd Python-3.11.1
 ./configure --prefix=/usr/local/bin/python3
 make
 make install
